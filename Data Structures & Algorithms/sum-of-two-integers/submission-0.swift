6532a858cede857eb7fe5240ded536925f9c6f48
Shalini Sah <shalinisah6692@gmail.com>
2026-08-18 17:07:04 -0700

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func getSum(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let carry = (a & b) << 1
+            a = a ^ b
+            b = carry
+        }
+
+        return a
+    }
+}
